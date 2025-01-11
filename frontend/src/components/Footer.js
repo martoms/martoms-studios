fd1ed441e5e159bb675ff16312bdd76b57f4eba7
martomz <tomataomarjohn@gmail.com>
2025-01-11 20:53:24 +0800

    Update Jan 2025 Data

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,73 +1,99 @@
-import { Link } from 'react-router-dom';
-import github from '../images/f-github.svg'
-import linkedin from '../images/f-linkedin.svg'
-import upwork from '../images/f-upwork.svg'
-import phone from '../images/f-phone.svg'
-import email from '../images/f-email.svg'
+import { Link } from "react-router-dom";
+import github from "../images/f-github.svg";
+import linkedin from "../images/f-linkedin.svg";
+import upwork from "../images/f-upwork.svg";
+import phone from "../images/f-phone.svg";
+import email from "../images/f-email.svg";
 
 const Footer = () => {
+  const year = new Date(Date.now()).toLocaleString("en-US", {
+    year: "numeric",
+  });
 
-    const year = new Date(Date.now()).toLocaleString('en-US', { year: 'numeric' })
-
-    return ( 
-        <>
-        <div className="row prefooter">
-            <div className="col-12 col-md-5">
-                <div className="row">
-                    <div className="col-12 travel-plans">
-                        <h4>Sitemap</h4>
-                        <Link to='/projects/web-development'>
-                            <p>Web-Dev Projects</p>
-                        </Link>
-                        <Link to='/skills/web-development'>
-                            <p>Web-Dev Skills</p>
-                        </Link>
-                        <Link to='/projects/graphic-design'>
-                            <p>Graphic Design Projects</p>
-                        </Link>
-                        <Link to='/skills/graphic-design'>
-                            <p>Graphic Design Skills</p>
-                        </Link>
-                        <Link to='/about'>
-                            <p>About</p>
-                        </Link>
-                        <Link to='/contact'>
-                            <p>Contact</p>
-                        </Link>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="row prefooter">
+        <div className="col-12 col-md-5">
+          <div className="row">
+            <div className="col-12 travel-plans">
+              <h4>Sitemap</h4>
+              <Link to="/projects/web-development">
+                <p>Web-Dev Projects</p>
+              </Link>
+              <Link to="/skills/web-development">
+                <p>Web-Dev Skills</p>
+              </Link>
+              <Link to="/projects/graphic-design">
+                <p>Graphic Design Projects</p>
+              </Link>
+              <Link to="/skills/graphic-design">
+                <p>Graphic Design Skills</p>
+              </Link>
+              <Link to="/about">
+                <p>About</p>
+              </Link>
+              <Link to="/contact">
+                <p>Contact</p>
+              </Link>
             </div>
-            <div className="col-12 col-md-2">
-                <div className='row'>
-                    <div className="col-12 links">
-                        <h4>Links</h4>
-                        <Link to="https://github.com/martoms" target="_blank"><p><img src={github} alt="github" />GitHub</p></Link>
-                        <Link to="https://www.linkedin.com/in/m-tomatao/" target="_blank"><p><img src={linkedin} alt="linkedin" />LinkedIn</p></Link>
-                        {/* <Link to="https://www.facebook.com/martomsstudios" target="_blank"><p><img src={fb} alt="linkedin" />Facebook</p></Link> */}
-                        <Link to="https://www.upwork.com/freelancers/~010e1495f770d6ef34" target="_blank"><p><img src={upwork} alt="upwork" />Upwork</p></Link>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-5">
-                <div className="row">
-                    <div className="col-12 col-lg-7 contacts">
-                        <h4>Contacts</h4>
-                        <p><img src={phone} alt="phone" />+639991130968</p>
-                        <a href="mailto:tomataomarjohn@gmail.com"><p><img src={email} alt="email" />tomataomarjohn@gmail.com</p></a>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+        <div className="col-12 col-md-2">
+          <div className="row">
+            <div className="col-12 links">
+              <h4>Links</h4>
+              <Link to="https://github.com/martoms" target="_blank">
+                <p>
+                  <img src={github} alt="github" />
+                  GitHub
+                </p>
+              </Link>
+              <Link to="https://www.linkedin.com/in/m-tomatao/" target="_blank">
+                <p>
+                  <img src={linkedin} alt="linkedin" />
+                  LinkedIn
+                </p>
+              </Link>
+              {/* <Link to="https://www.facebook.com/martomsstudios" target="_blank"><p><img src={fb} alt="linkedin" />Facebook</p></Link> */}
+              <Link
+                to="https://www.upwork.com/freelancers/~010e1495f770d6ef34"
+                target="_blank"
+              >
+                <p>
+                  <img src={upwork} alt="upwork" />
+                  Upwork
+                </p>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-5">
+          <div className="row">
+            <div className="col-12 col-lg-7 contacts">
+              <h4>Contacts</h4>
+              <p>
+                <img src={phone} alt="phone" />
+                +63105583385
+              </p>
+              <a href="mailto:tomataomarjohn@gmail.com">
+                <p>
+                  <img src={email} alt="email" />
+                  tomataomarjohn@gmail.com
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <footer>
+        <p>martoms_studios</p>
+        <p>Marjohn Tomatao | Software Engineer | MERN Stack</p>
+        <p>Copyright &copy; {year}. All Rights Reserved</p>
+        <hr />
+      </footer>
+    </>
+  );
+};
 
-        <footer>
-            <p>martoms_studios</p>
-            <p>Marjohn Tomatao | Full Stack Web Developer | MERN Stack</p>
-            <p>Copyright &copy; { year }. All Rights Reserved</p>
-            <hr />
-        </footer>
-        </>
-    );
-}
- 
 export default Footer;
--- frontend/src/pages/AboutMe.js
+++ frontend/src/pages/AboutMe.js
@@ -1,134 +1,185 @@
-import profilepic from '../images/profilepic.webp'
-import placeholder from '../images/placeholder.webp'
-import { Button, Breadcrumb, Dropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Contact from '../components/Contact';
-import DownloadFile from '../components/DownloadFile';
-const resume = 'https://martoms-studios.vercel.app/Tomatao-Marjohn-CV.pdf';
+import profilepic from "../images/profilepic.webp";
+import placeholder from "../images/placeholder.webp";
+import { Button, Breadcrumb, Dropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Contact from "../components/Contact";
+import DownloadFile from "../components/DownloadFile";
+const resume = "https://martoms-studios.vercel.app/Tomatao_Marjohn_Resume.pdf";
 
 const AboutMe = () => {
+  const { download } = DownloadFile();
 
-    const { download } = DownloadFile();
+  return (
+    <div id="about-me">
+      <Breadcrumb>
+        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>About</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="about-me-content">
+        <div className="about-me-pic">
+          {<img src={profilepic} alt="profilepic" /> || (
+            <img src={placeholder} alt="placeholder" />
+          )}
+          <div className="d-none d-lg-block">
+            <hr />
+            <div className="cert-btns">
+              <Button
+                as={Link}
+                to="https://share.zertify.zuitt.co/certificate/6b4d58f0-cc66-4b3b-96f6-5f2936d3ba71/"
+                target="_blank"
+              >
+                MERN Full Stack Certificate
+              </Button>
+              <Dropdown className="dropdown-btn">
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  Short Course Certificates
+                </Dropdown.Toggle>
 
-    return (
-        <div id="about-me">
-            <Breadcrumb>
-                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                <Breadcrumb.Item active>About</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className='about-me-content'>
-                <div className='about-me-pic'>
-                    {
-                        <img src={profilepic} alt="profilepic" /> ||
-                        <img src={placeholder} alt="placeholder" />
-                    }
-                    <div className='d-none d-lg-block'>
-                        <hr />
-                        <div className='cert-btns'>
-                            <Button as={Link} to='https://share.zertify.zuitt.co/certificate/6b4d58f0-cc66-4b3b-96f6-5f2936d3ba71/' target="_blank">
-                                MERN Full Stack Certificate
-                            </Button>
-                            <Dropdown className='dropdown-btn'>
-                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                                    Short Course Certificates
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/56f8b561-90b9-4716-9e0b-addf894c5043/"
-                                        target="_blank">
-                                    AWS Fundamentals
-                                    </Dropdown.Item>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/923a66e6-c4d0-4fcc-b79d-7dbfae22c930/"
-                                        target="_blank">
-                                    AWS Serverless
-                                    </Dropdown.Item>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/d4e39626-dc0b-4218-a533-099dc1f9e56b/"
-                                        target="_blank">
-                                    Unit Test
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Button as={Link} to='/about/best-in-capstone-2' target='_blank'>
-                                Best in Capstone 2
-                            </Button>
-                            <Button as={Link} to='/about/best-in-capstone-3' target='_blank'>
-                                Best in Capstone 3
-                            </Button>
-                            <Button variant="success" onClick={() => download(resume)} style={{fontWeight: '900'}}>
-                                Download CV
-                            </Button>
-                        </div>
-                        <hr />
-                        
-                    </div>
-                </div>
-                <div className='about-me-details'>
-                    <h1>Marjohn Tomatao</h1>
-                    <div className="titles">
-                        <h3>Full Stack Web Developer</h3>
-                        <h3>Graphic Designer</h3>
-                    </div>
-                    <p className='about-me-description'>
-                        I am self-taught as a graphic designer since college and I am currently employed at Father Saturnino Urios University in Butuan City as a Graphic Designer and as a Strategic Communications Staff.<br /><br />I learned computer programming and earned a certificate in Full Stack Web Development at Zuitt Coding Bootcamp from May to August 2023. Moreover, during the said bootcamp, I earned two out of three major awards, namely the Capstones&nbsp;2&nbsp;&&nbsp;3 which showcase my exceptional skills in creating a backend web server and a fullstack web app respectively.<br /><br />I am also open to offer my service as a fulltime web developer.
-                    </p>
-                    <div className='personal-info'>
-                        <span>LinkedIn: </span>
-                        <Link to='https://www.linkedin.com/in/m-tomatao/' target='_blank'>
-                            https://www.linkedin.com/in/m-tomatao/
-                        </Link><br />
-                        <p><span>Email: </span><a href="mailto:tomataomarjohn@gmail.com">tomataomarjohn@gmail.com</a></p>
-                        <p><span>Address: </span>Buenavista, Agusan del Norte, PH</p>
-                        <p><span>Phone: </span>+639991130968</p>
-                    </div>                    
-                    <div className='d-lg-none'>
-                        <hr />
-                        <div className='cert-btns'>
-                            <Button as={Link} to='https://share.zertify.zuitt.co/certificate/20315512-5638-4257-bada-4ae0ff6e218a/' target='_blank'>
-                                View Certificate
-                            </Button>
-                            <Dropdown className='dropdown-btn'>
-                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                                    Short Course Certificates
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/56f8b561-90b9-4716-9e0b-addf894c5043/"
-                                        target="_blank">
-                                    AWS Fundamentals
-                                    </Dropdown.Item>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/923a66e6-c4d0-4fcc-b79d-7dbfae22c930/"
-                                        target="_blank">
-                                    AWS Serverless
-                                    </Dropdown.Item>
-                                    <Dropdown.Item
-                                        href="https://share.zertify.zuitt.co/certificate/d4e39626-dc0b-4218-a533-099dc1f9e56b/"
-                                        target="_blank">
-                                    Unit Test
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Button as={Link} to='/skills/web-development/best-in-capstone-2' target='_blank'>
-                                Best in Capstone 2
-                            </Button>
-                            <Button as={Link} to='/skills/web-development/best-in-capstone-3' target='_blank'>
-                                Best in Capstone 3
-                            </Button>
-                            <Button variant="success" onClick={() => download(resume)} style={{fontWeight: '900'}}>
-                                Download CV
-                            </Button>
-                        </div>
-                        <hr />
-                    </div>
-                </div>
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/56f8b561-90b9-4716-9e0b-addf894c5043/"
+                    target="_blank"
+                  >
+                    AWS Fundamentals
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/923a66e6-c4d0-4fcc-b79d-7dbfae22c930/"
+                    target="_blank"
+                  >
+                    AWS Serverless
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/d4e39626-dc0b-4218-a533-099dc1f9e56b/"
+                    target="_blank"
+                  >
+                    Unit Test
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button as={Link} to="/about/best-in-capstone-2" target="_blank">
+                Best in Capstone 2
+              </Button>
+              <Button as={Link} to="/about/best-in-capstone-3" target="_blank">
+                Best in Capstone 3
+              </Button>
+              <Button
+                variant="success"
+                onClick={() => download(resume)}
+                style={{ fontWeight: "900" }}
+              >
+                Download CV
+              </Button>
             </div>
-            <Contact />
+            <hr />
+          </div>
         </div>
-    );
-}
- 
+        <div className="about-me-details">
+          <h1>Marjohn Tomatao</h1>
+          <div className="titles">
+            <h3>Software Engineer | Graphic Designer</h3>
+          </div>
+          <p className="about-me-description">
+            As a passionate and detail-oriented full-stack developer, I
+            specialize in crafting robust and scalable web applications. My
+            expertise lies in leveraging the power of the Scala Play Framework
+            or Node.js for backend development, building interactive and
+            user-friendly frontends with Vue/Nuxt, and managing data seamlessly
+            with PostgreSQL or MongoDB.
+            <br />
+            <br />
+            I thrive on creating efficient and innovative solutions that bridge
+            user needs with technical excellence. Whether it's designing
+            responsive user interfaces, optimizing server-side performance, or
+            ensuring data integrity, I am committed to delivering high-quality
+            results that make an impact.
+            <br />
+            <br />I am also open to offer my service as a freelance web
+            developer.
+          </p>
+          <div className="personal-info">
+            <span>LinkedIn: </span>
+            <Link to="https://www.linkedin.com/in/m-tomatao/" target="_blank">
+              https://www.linkedin.com/in/m-tomatao/
+            </Link>
+            <br />
+            <p>
+              <span>Email: </span>
+              <a href="mailto:tomataomarjohn@gmail.com">
+                tomataomarjohn@gmail.com
+              </a>
+            </p>
+            <p>
+              <span>Address: </span>Talamban, Cebu City
+            </p>
+            <p>
+              <span>Phone: </span>+63105583385
+            </p>
+          </div>
+          <div className="d-lg-none">
+            <hr />
+            <div className="cert-btns">
+              <Button
+                as={Link}
+                to="https://share.zertify.zuitt.co/certificate/20315512-5638-4257-bada-4ae0ff6e218a/"
+                target="_blank"
+              >
+                View Certificate
+              </Button>
+              <Dropdown className="dropdown-btn">
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  Short Course Certificates
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/56f8b561-90b9-4716-9e0b-addf894c5043/"
+                    target="_blank"
+                  >
+                    AWS Fundamentals
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/923a66e6-c4d0-4fcc-b79d-7dbfae22c930/"
+                    target="_blank"
+                  >
+                    AWS Serverless
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="https://share.zertify.zuitt.co/certificate/d4e39626-dc0b-4218-a533-099dc1f9e56b/"
+                    target="_blank"
+                  >
+                    Unit Test
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button
+                as={Link}
+                to="/skills/web-development/best-in-capstone-2"
+                target="_blank"
+              >
+                Best in Capstone 2
+              </Button>
+              <Button
+                as={Link}
+                to="/skills/web-development/best-in-capstone-3"
+                target="_blank"
+              >
+                Best in Capstone 3
+              </Button>
+              <Button
+                variant="success"
+                onClick={() => download(resume)}
+                style={{ fontWeight: "900" }}
+              >
+                Download CV
+              </Button>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
+      <Contact />
+    </div>
+  );
+};
+
 export default AboutMe;
